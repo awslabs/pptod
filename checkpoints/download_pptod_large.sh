207ee3fe9846d1950b930f82fce1e44dda896212
Yixuan Su <syxistheone@gmail.com>
2021-09-23 14:30:44 +0100

    Add files via upload

---
+++ checkpoints/download_pptod_large.sh
@@ -0,0 +1,3 @@
+gdown "https://drive.google.com/u/1/uc?export=download&confirm=C24k&id=1rz7LMxxZA7OO96Iy0U3oVzqLbdZERoB5"
+unzip large.zip
+rm large.zip
---
+++ checkpoints/download_pptod_small.sh
@@ -0,0 +1,3 @@
+gdown "https://drive.google.com/u/1/uc?export=download&confirm=QI8F&id=1rZ81AJJCRzJM2pbpCkvLKCJKwweAZ1Gr"
+unzip small.zip
+rm small.zip
