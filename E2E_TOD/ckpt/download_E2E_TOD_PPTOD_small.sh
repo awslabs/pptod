9f08cfe3dea0d799bcf389768454f52f18a96f8b
Yixuan Su <syxistheone@gmail.com>
2021-09-24 11:15:25 +0100

    Delete download_E2E_TOD_PPTOD_small.sh

--- E2E_TOD/ckpt/download_E2E_TOD_PPTOD_small.sh
+++
@@ -1,5 +0,0 @@
-gdown "https://drive.google.com/u/1/uc?export=download&confirm=3gY1&id=1o2pi8Oe6pdirxvCllf260GFOuKLfXzsu"
-mv epoch_9_best_ckpt.zip ./small/full_training/
-cd ./small/full_training/
-unzip epoch_9_best_ckpt.zip
-rm epoch_9_best_ckpt.zip
