b14fbed62de1d2057b12382ccb089d8bdd59f54e
Yixuan Su <syxistheone@gmail.com>
2021-09-24 12:42:29 +0100

    Delete download_DST_PPTOD_base.sh

--- DST/ckpt/download_DST_PPTOD_base.sh
+++
@@ -1,5 +0,0 @@
-gdown "https://drive.google.com/u/1/uc?export=download&confirm=JR9v&id=108vA2eLBt6hPBT_9jXC5_lX0H2V2iwCq"
-mv epoch_4_best_ckpt.zip ./base/full_training/
-cd ./base/full_training/
-unzip epoch_4_best_ckpt.zip
-rm epoch_4_best_ckpt.zip
