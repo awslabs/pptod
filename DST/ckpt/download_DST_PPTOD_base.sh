e6976a4b368833d2ff3d4d1034a61ca669ad3dcd
Yixuan Su <syxistheone@gmail.com>
2021-09-24 12:42:55 +0100

    Add files via upload

---
+++ DST/ckpt/download_DST_PPTOD_base.sh
@@ -0,0 +1,5 @@
+wget https://pptod.s3.amazonaws.com/DST/epoch_4_best_ckpt.zip
+mv epoch_4_best_ckpt.zip ./base/full_training/
+cd ./base/full_training/
+unzip epoch_4_best_ckpt.zip
+rm epoch_4_best_ckpt.zip
---
+++ DST/ckpt/download_DST_PPTOD_large.sh
@@ -0,0 +1,8 @@
+wget https://pptod.s3.amazonaws.com/DST/epoch_10_best_ckpt.zip
+mv epoch_10_best_ckpt.zip ./large/full_training/
+cd ./large/full_training/
+unzip epoch_10_best_ckpt.zip
+rm epoch_10_best_ckpt.zip
+
+
+
