e591bd202eebe07b25ce7e7d911c34b4e3d9cb4f
Yixuan Su <syxistheone@gmail.com>
2021-09-24 11:16:06 +0100

    Add files via upload

---
+++ E2E_TOD/ckpt/download_E2E_TOD_PPTOD_small.sh
@@ -0,0 +1,5 @@
+wget https://pptod.s3.amazonaws.com/E2E/epoch_9_best_ckpt.zip
+mv epoch_9_best_ckpt.zip ./small/full_training/
+cd ./small/full_training/
+unzip epoch_9_best_ckpt.zip
+rm epoch_9_best_ckpt.zip
