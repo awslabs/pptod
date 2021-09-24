cc8885d17a65aaf08628cda8c148f5c900503d6c
Yixuan Su <syxistheone@gmail.com>
2021-09-24 12:49:11 +0100

    Delete download_IC_PPTOD_small.sh

--- IC/ckpt/download_IC_PPTOD_small.sh
+++
@@ -1,5 +0,0 @@
-gdown "https://drive.google.com/u/1/uc?export=download&confirm=F5J6&id=1AYB5snpx8uqKeZOF5RDr_VUskCAGWFVG"
-mv epoch_50_best_ckpt.zip ./small/full_training/
-cd ./small/full_training/
-unzip epoch_50_best_ckpt.zip
-rm epoch_50_best_ckpt.zip
