a7fa769f23f4931dfdf870cec495d5e86c525e1d
Yixuan Su <syxistheone@gmail.com>
2021-09-24 12:42:35 +0100

    Delete download_DST_PPTOD_large.sh

--- DST/ckpt/download_DST_PPTOD_large.sh
+++
@@ -1,8 +0,0 @@
-gdown "https://drive.google.com/u/1/uc?export=download&confirm=bMb6&id=1VLqZCglGXBXl5ijg3XHgcrGN4tTzqmCn"
-mv epoch_10_best_ckpt.zip ./large/full_training/
-cd ./large/full_training/
-unzip epoch_10_best_ckpt.zip
-rm epoch_10_best_ckpt.zip
-
-
-
