506be59eb4b407871a0e96086c55e78f8f425032
Yixuan Su <syxistheone@gmail.com>
2021-09-24 11:15:35 +0100

    Delete download_E2E_TOD_PPTOD_base.sh

--- E2E_TOD/ckpt/download_E2E_TOD_PPTOD_base.sh
+++
@@ -1,5 +0,0 @@
-gdown "https://drive.google.com/u/1/uc?export=download&confirm=Ojzi&id=172L5_GNFuWjcfBVdI4Tto7dAiYL4bYRE"
-mv epoch_6_best_ckpt.zip ./base/full_training/
-cd ./base/full_training/
-unzip epoch_6_best_ckpt.zip
-rm epoch_6_best_ckpt.zip
