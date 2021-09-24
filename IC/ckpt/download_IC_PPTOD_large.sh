ab144c112af642b8f1ca17b6aea1bcc53b917993
Yixuan Su <syxistheone@gmail.com>
2021-09-24 12:49:05 +0100

    Delete download_IC_PPTOD_large.sh

--- IC/ckpt/download_IC_PPTOD_large.sh
+++
@@ -1,5 +0,0 @@
-gdown "https://drive.google.com/u/1/uc?export=download&confirm=hn2z&id=1NKCxORsjp4zca2YBnRhpbLWmIFBtTT9O"
-mv epoch_70_best_ckpt.zip ./large/full_training/
-cd ./large/full_training/
-unzip epoch_70_best_ckpt.zip
-rm epoch_70_best_ckpt.zip
